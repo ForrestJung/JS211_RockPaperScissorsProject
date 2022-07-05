73fa79dd0cca51d1641baa2a39ca02fcedcbcd78
ForrestJung <jungforrest@gmail.com>
2022-07-05 15:14:34 -0500

    Rock Paper Scissors JS

--- main.js
+++ main.js
@@ -1,62 +1,31 @@
-// uses strict mode so strings are not coerced, variables are not hoisted, etc... 
-'use strict';
+  // Should Test Which Hand Won
+  // Should detect a tie
+  // Should scrub input to ensure lowercase with trimmed whitespace
+  const rockPaperScissors = () => {
 
-// brings in the assert module for unit testing
-const assert = require('assert');
-// brings in the readline module to access the command line
-const readline = require('readline');
-// use the readline module to print out to the command line
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
-
-// the function that will be called by the unit test below
-const rockPaperScissors = (hand1, hand2) => {
-
-  // Write code here
-  // Use the unit test to see what is expected
-
-}
-
-// the first function called in the program to get an input from the user
-// to run the function use the command: node main.js
-// to close it ctrl + C
-function getPrompt() {
-  rl.question('hand1: ', (answer1) => {
-    rl.question('hand2: ', (answer2) => {
-      console.log( rockPaperScissors(answer1, answer2) );
-      getPrompt();
-    });
-  });
-}
-
-// Unit Tests
-// to use them run the command: npm test main.js
-// to close them ctrl + C
-if (typeof describe === 'function') {
-
-  // most are notes for human eyes to read, but essentially passes in inputs then compares if the function you built returns the expected output.
-  describe('#rockPaperScissors()', () => {
-    it('should detect a tie', () => {
-      assert.equal(rockPaperScissors('rock', 'rock'), "It's a tie!");
-      assert.equal(rockPaperScissors('paper', 'paper'), "It's a tie!");
-      assert.equal(rockPaperScissors('scissors', 'scissors'), "It's a tie!");
-    });
-    it('should detect which hand won', () => {
-      assert.equal(rockPaperScissors('rock', 'paper'), "Hand two wins!");
-      assert.equal(rockPaperScissors('paper', 'scissors'), "Hand two wins!");
-      assert.equal(rockPaperScissors('rock', 'scissors'), "Hand one wins!");
-    });
-    it('should scrub input to ensure lowercase with "trim"ed whitepace', () => {
-      assert.equal(rockPaperScissors('rOcK', ' paper '), "Hand two wins!");
-      assert.equal(rockPaperScissors('Paper', 'SCISSORS'), "Hand two wins!");
-      assert.equal(rockPaperScissors('rock ', 'sCiSsOrs'), "Hand one wins!");
-    });
-  });
-} else {
-
-  // always returns ask the user for another input
-  getPrompt();
-
+  let hand1 = prompt("Player 1: Rock, Paper, or scissors?")
+  let hand2 = prompt("Player 2: Rock, Paper, or Scissors?")
+  let userOne = hand1.toLowerCase().trim();
+  let userTwo = hand2.toLowerCase().trim();
+  
+  if (userOne === userTwo) {
+    // It's a tie
+    document.getElementById("display-element").innerHTML = (`It's a tie!`)
+    console.log("It's a tie!")
+    // return "It's a tie!"
+  } else if ((userOne == 'rock' && userTwo == 'scissors') || (userOne == 'scissors' && userTwo == 'paper') || (userOne == 'paper' && userTwo == 'rock')) {
+    // userOne Wins
+    document.getElementById("display-element").innerHTML = (`Player one wins!`)
+    console.log("Hand1 Wins!")
+    // return "Hand one wins!"
+  } else if ((userTwo == 'rock' && userOne == 'scissors') || (userTwo == 'scissors' && userOne == 'paper') || (userTwo == 'paper' && userOne == 'rock')){
+    // userTwo Wins
+    document.getElementById("display-element").innerHTML = (`Player two wins!`)
+    console.log("Hand2 Wins!")
+    // return "Hand two wins!"
+  } else {
+    document.getElementById("display-element").innerHTML = (`(${hand1} or ${hand2}) is invalid. No winner detected.`)
+    console.log("Error, no winner detected.")
+    // return "Error, no winner detected."
+  }
 }
